a6bd02d869e53bb026fdb12ed711e501cb4413a1
Enrique Collazo <ecollazo1991@gmail.com>
2021-01-01 21:23:40 -0500

    validates whether a user has selected password character options

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//user will decide how many characters they want in their password
 var pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
 
 while(pwlength <= 7 || pwlength >=128) {
@@ -10,10 +11,23 @@ while(pwlength <= 7 || pwlength >=128) {
 
 alert("Your password will be comprised of " + pwlength + " characters.");
 
+console.log(pwlength)
+
 var confirmnumbers = confirm("Include numbers in this password?");
 var confirmUppercase = confirm("Include Uppercase lettering in this password?");
 var confirmLowercase = confirm("Include Lowercase lettering in this password?");
 var confirmSpecial = confirm("Include special characters in this password?");
+
+//validates whether user selects options for password characters
+
+while(confirmnumbers === false && confirmUppercase === false && confirmLowercase === false && confirmSpecial === false){
+  alert("You must choose at least one criteria")
+  var confirmnumbers = confirm("Include numbers in this password?");
+  var confirmUppercase = confirm("Include Uppercase lettering in this password?");
+  var confirmLowercase = confirm("Include Lowercase lettering in this password?");
+  var confirmSpecial = confirm("Include special characters in this password?");
+}
+
 
 
 
@@ -29,15 +43,6 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-//if (pwlength > 8) {
-//  alert('This needs to be atleast 8 characters');
-//  return;
-//}
-//else if (pwlength < 128) {
-//  alert('This needs to be shorter than 128 characters');
-//  return;
-//}
-//else {}
 
 //var Numeric = "0,1,2,3,4,5,6,7,8,9";
 //var Uppercase = "'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'";
