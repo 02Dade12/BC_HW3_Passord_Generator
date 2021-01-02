0bb228b7419b299d2598cac934ad6d4a5dfd95b4
Enrique Collazo <ecollazo1991@gmail.com>
2021-01-02 13:16:53 -0500

    progressed code base

--- script.js
+++ script.js
@@ -1,35 +1,83 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+var pwlength
+var confirmNumbers;
+var confirmUppercase;
+var confirmLowercase;
+var confirmSpecial;
+
 //user will decide how many characters they want in their password
-var pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
+pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
 
 while(pwlength <= 7 || pwlength >=128) {
   alert("Password much be between 8-128 characters long, try again");
-  var pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
-}
+  pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
+};
 
 alert("Your password will be comprised of " + pwlength + " characters.");
 
-console.log(pwlength)
+//confirms use of casing, numbers, & special characters
+confirmNumbers = confirm("Include numbers in this password?");
+confirmUppercase = confirm("Include Uppercase lettering in this password?");
+confirmLowercase = confirm("Include Lowercase lettering in this password?");
+confirmSpecial = confirm("Include special characters in this password?");
 
-var confirmnumbers = confirm("Include numbers in this password?");
-var confirmUppercase = confirm("Include Uppercase lettering in this password?");
-var confirmLowercase = confirm("Include Lowercase lettering in this password?");
-var confirmSpecial = confirm("Include special characters in this password?");
-
-//validates whether user selects options for password characters
-
-while(confirmnumbers === false && confirmUppercase === false && confirmLowercase === false && confirmSpecial === false){
+//validates whether user selected options for password characters
+while(confirmNumbers === false && confirmUppercase === false && confirmLowercase === false && confirmSpecial === false){
   alert("You must choose at least one criteria")
-  var confirmnumbers = confirm("Include numbers in this password?");
-  var confirmUppercase = confirm("Include Uppercase lettering in this password?");
-  var confirmLowercase = confirm("Include Lowercase lettering in this password?");
-  var confirmSpecial = confirm("Include special characters in this password?");
+  confirmNumbers = confirm("Include numbers in this password?");
+  confirmUppercase = confirm("Include Uppercase lettering in this password?");
+  confirmLowercase = confirm("Include Lowercase lettering in this password?");
+  confirmSpecial = confirm("Include special characters in this password?");
+};
+
+//Console Log - confirm choices
+console.log("Password Length is " + pwlength);
+console.log("Add numbers is " + confirmNumbers);
+console.log("Add upper casing is " + confirmUppercase);
+console.log("Add lower casing is " + confirmLowercase);
+console.log("Add special characters is " + confirmSpecial);
+
+////////////
+function generatePassword(pwlength) {
+
+var character = "";
+var lower = "abcdefghijklmnopqrstuvwxyz";
+var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var special = "!@#$%^&*()-_=+;:',<.>/?";
+var numbers = "0123456789";
+var all = "";
+if (confirmLowercase) {
+  all = all + lower;
+}
+if (confirmUppercase) {
+  all = all + upper;
+}
+if (confirmSpecial) {
+  all = all + special;
+}
+if (confirmNumbers) {
+  all = all + numbers;
 }
 
 
+//create variable that gets set when user hits generate
+var password = "";
 
+//create for loop to choose various passwords
+for (var i = 0; i < pwlength; i++) {    
+  chacracter = all.substring(Math.floor(Math.random() * all.length));
+  password += all.substring(character, character + 1);
+
+}
+return character;
+console.log(character)
+};
+
+document.getElementById("password").value = password;
+
+//////////// 
 
 // Write password to the #password input
 function writePassword() {
@@ -42,9 +90,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-
-
-//var Numeric = "0,1,2,3,4,5,6,7,8,9";
-//var Uppercase = "'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'";
-//var Lowercase = "'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'";
-//var Special = "'!','@','#','$','%','^','&','*','(',')',','_','+'";
