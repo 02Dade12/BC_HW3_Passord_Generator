8adb1d979bf4347d2dfda24133c80375f26c6ad6
Enrique Collazo <ecollazo1991@gmail.com>
2021-01-05 18:31:10 -0500

    code is fully functioning

--- script.js
+++ script.js
@@ -1,91 +1,79 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-var pwlength
-var confirmNumbers;
-var confirmUppercase;
-var confirmLowercase;
-var confirmSpecial;
+// Global Variables
+var pwlength = 0;
+var confirmNumbers = false;
+var confirmUppercase = false;
+var confirmLowercase = false;
+var confirmSpecial = false;
 
-//user will decide how many characters they want in their password
-pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
+// Function
+function generatePassword() {
 
-while(pwlength <= 7 || pwlength >=128) {
-  alert("Password much be between 8-128 characters long, try again");
-  pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
+  var character;
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var special = "!@#$%^&*()-_=+;:',<.>/?";
+  var numbers = "0123456789";
+  var all = "";
+  if (confirmLowercase) {
+    all = all + lower;
+  }
+  if (confirmUppercase) {
+    all = all + upper;
+  }
+  if (confirmSpecial) {
+    all = all + special;
+  }
+  if (confirmNumbers) {
+    all = all + numbers;
+  }
+
+  // Create variable that gets set when user hits generate
+  var password = "";
+
+  // Create for loop to choose various passwords
+  for (var i = 0; i < pwlength; i++) {
+    character = (Math.floor(Math.random() * all.length));
+    password += all.substring(character, character + 1);
+  }
+
+  return password;
 };
-
-alert("Your password will be comprised of " + pwlength + " characters.");
-
-//confirms use of casing, numbers, & special characters
-confirmNumbers = confirm("Include numbers in this password?");
-confirmUppercase = confirm("Include Uppercase lettering in this password?");
-confirmLowercase = confirm("Include Lowercase lettering in this password?");
-confirmSpecial = confirm("Include special characters in this password?");
-
-//validates whether user selected options for password characters
-while(confirmNumbers === false && confirmUppercase === false && confirmLowercase === false && confirmSpecial === false){
-  alert("You must choose at least one criteria")
-  confirmNumbers = confirm("Include numbers in this password?");
-  confirmUppercase = confirm("Include Uppercase lettering in this password?");
-  confirmLowercase = confirm("Include Lowercase lettering in this password?");
-  confirmSpecial = confirm("Include special characters in this password?");
-};
-
-//Console Log - confirm choices
-console.log("Password Length is " + pwlength);
-console.log("Add numbers is " + confirmNumbers);
-console.log("Add upper casing is " + confirmUppercase);
-console.log("Add lower casing is " + confirmLowercase);
-console.log("Add special characters is " + confirmSpecial);
-
-////////////
-function generatePassword(pwlength) {
-
-var character = "";
-var lower = "abcdefghijklmnopqrstuvwxyz";
-var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var special = "!@#$%^&*()-_=+;:',<.>/?";
-var numbers = "0123456789";
-var all = "";
-if (confirmLowercase) {
-  all = all + lower;
-}
-if (confirmUppercase) {
-  all = all + upper;
-}
-if (confirmSpecial) {
-  all = all + special;
-}
-if (confirmNumbers) {
-  all = all + numbers;
-}
-
-
-//create variable that gets set when user hits generate
-var password = "";
-
-//create for loop to choose various passwords
-for (var i = 0; i < pwlength; i++) {    
-  chacracter = all.substring(Math.floor(Math.random() * all.length));
-  password += all.substring(character, character + 1);
-
-}
-return character;
-console.log(character)
-};
-
-document.getElementById("password").value = password;
-
-//////////// 
 
 // Write password to the #password input
 function writePassword() {
+
+  //user will decide how many characters they want in their password
+  pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
+
+  while (pwlength <= 7 || pwlength >= 128 || isNaN(pwlength)) {
+    alert("Password much be between 8-128 characters long, try again");
+    pwlength = parseInt(prompt("How long do you want your new password? Choose beteewn 8-128 characters."));
+  };
+
+  alert("Your password will be comprised of " + pwlength + " characters.");
+
+  //validates whether user selected options for password characters
+  while (confirmNumbers === false && confirmUppercase === false && confirmLowercase === false && confirmSpecial === false) {
+    alert("You must choose at least one criteria")
+    confirmNumbers = confirm("Include numbers in this password?");
+    confirmUppercase = confirm("Include Uppercase lettering in this password?");
+    confirmLowercase = confirm("Include Lowercase lettering in this password?");
+    confirmSpecial = confirm("Include special characters in this password?");
+  };
+
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
 
+    pwlength = 0;
+    confirmNumbers = false;
+    confirmUppercase = false;
+    confirmLowercase = false;
+    confirmSpecial = false;
 }
 
 // Add event listener to generate button
